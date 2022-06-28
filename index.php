3479a65a29995444b471c3d0f1ffe2d73c8842bf
hectorrdez <hectorrdez@gmail.com>
2022-06-28 12:10:31 +0200

    esqueleto principal + loader

--- index.php
+++ index.php
@@ -1,12 +1,17 @@
 <!DOCTYPE html>
-<html lang="es">
+<html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bumerania</title>
+    <title>Bumerania Robotics - Robótica Social</title>
+    <?php include_once './resources/style/loader.php' ?>
 </head>
+
 <body>
-    
+    <?php include_once './resources/includes/header.php' ?>
+    <?php include_once './resources/includes/footer.php' ?>
 </body>
+
 </html>
