8f23119fa2545acdf66a721f56b54ae261b04a4a
pine <antonioblancocordoba96@gmail.com>
2022-06-28 14:52:49 +0200

    Estilos a SASS

--- resources/includes/header.php
+++ resources/includes/header.php
@@ -2,8 +2,8 @@
     <a href="#"><img src="../resources/img/logo-blanco-cortado.png" alt="Bumerania logo" class="logo"></a>
     <nav>
         <a href="#">INICIO</a >
-        <a href="#">SOLUCIONES <img src="../resources/icons/caret-down-solid.svg" alt=""></a >
-        <a href="#">NOSOTROS <img src="../resources/icons/caret-down-solid.svg" alt=""></a >
+        <a href="#">SOLUCIONES <img src="../resources/icons/caret-down-solid.svg"></a >
+        <a href="#">NOSOTROS </a >
         <a href="#">SALA DE PRENSA</a >
         <a href="#">CONTACTO</a >
         <a href="#">BLOG</a >
