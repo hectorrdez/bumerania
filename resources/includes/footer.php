b5bc9814aa8710784cb1c4f17db7fa7936fc5b3d
PatxiSempere <tute_vdv@hotmail.com>
2022-06-28 12:13:57 +0200

    Footer

---
+++ resources/includes/footer.php
@@ -0,0 +1,46 @@
+<footer>
+    <div>
+        <ul>
+            <li><span>Bumerania</span>
+                <ul>
+                    <li>Contacto</li>
+                    <li>Sala de prensa</li>
+                    <li>Casos de éxito</li>
+                    <li>Blog</li>
+                    <li>Bumerania Merchandise</li>
+                    <li>Área de Cliente</li>
+                </ul>
+            </li>
+            <li>
+                <ul><span>Servicios</span>
+                    <li>Hostelería</li>
+                    <li>Restauración</li>
+                    <li>Salud</li>
+                    <li>Seguridad</li>
+                </ul>
+            </li>
+            <li>
+                <ul>Soluciones
+                    <li>Robot camarero</li>
+                    <li>Temi</li>
+                    <li>SmartTables</li>
+                    <li>Ares</li>
+                </ul>
+            </li>
+            <li>
+                Soporte
+            </li>
+            <li>
+                <ul>Delegaciones
+                    <li>Abu Dabi</li>
+                    <li>Argentina</li>
+                    <li>España</li>
+                    <li>Paraguay</li>
+                    <li>Portugal</li>
+                    <li>Reino Unido</li>
+                    <li>Suiza</li>
+                </ul>
+            </li>
+        </ul>
+    </div>
+</footer>
