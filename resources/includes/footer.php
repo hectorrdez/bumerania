5673a3874b90f7dc767d0dc04b1f377a2bfcb4d1
PatxiSempere <tute_vdv@hotmail.com>
2022-06-28 13:37:18 +0200

    FOOTER

--- resources/includes/footer.php
+++ resources/includes/footer.php
@@ -1,46 +1,39 @@
 <footer>
     <div>
-        <ul>
-            <li><span>Bumerania</span>
-                <ul>
-                    <li>Contacto</li>
-                    <li>Sala de prensa</li>
-                    <li>Casos de éxito</li>
-                    <li>Blog</li>
-                    <li>Bumerania Merchandise</li>
-                    <li>Área de Cliente</li>
-                </ul>
-            </li>
-            <li>
-                <ul><span>Servicios</span>
-                    <li>Hostelería</li>
-                    <li>Restauración</li>
-                    <li>Salud</li>
-                    <li>Seguridad</li>
-                </ul>
-            </li>
-            <li>
-                <ul>Soluciones
-                    <li>Robot camarero</li>
-                    <li>Temi</li>
-                    <li>SmartTables</li>
-                    <li>Ares</li>
-                </ul>
-            </li>
-            <li>
-                Soporte
-            </li>
-            <li>
-                <ul>Delegaciones
-                    <li>Abu Dabi</li>
-                    <li>Argentina</li>
-                    <li>España</li>
-                    <li>Paraguay</li>
-                    <li>Portugal</li>
-                    <li>Reino Unido</li>
-                    <li>Suiza</li>
-                </ul>
-            </li>
-        </ul>
+        <span class='headerFooter'>Bumerania</span>
+        <div class='itemFooter'><a href='#' class='aFooter'>Contacto</a></div>
+        <div class='itemFooter'><a href='#' class='aFooter'>Sala de prensa</a></div>
+        <div class='itemFooter'><a href='#' class='aFooter'>Casos de éxito</a></div>
+        <div class='itemFooter'><a href='#' class='aFooter'>Blog</a></div>
+        <div class='itemFooter'><a href='#' class='aFooter'>Bumerania Merchandise</div>
+        <div class='itemFooter'><a href='#' class='aFooter'>Área de Cliente</a></div>
+    </div>
+    <div><span class='headerFooter'>Servicios</span>
+
+        <div class='itemFooter'><a href='#' class='aFooter'>Hostelería</a></div>
+        <div class='itemFooter'><a href='#' class='aFooter'>Restauración</a></div>
+        <div class='itemFooter'><a href='#' class='aFooter'>Salud</a></div>
+        <div class='itemFooter'><a href='#' class='aFooter'>Seguridad</a></div>
+    </div>
+    <div><span class='headerFooter'>Soluciones</span>
+
+        <div class='itemFooter'><a href='#' class='aFooter'>Robot camarero</div>
+        <div class='itemFooter'><a href='#' class='aFooter'>Temi</a></div>
+        <div class='itemFooter'><a href='#' class='aFooter'>SmartTables</a></div>
+        <div class='itemFooter'><a href='#' class='aFooter'>Ares</a></div>
+    </div>
+    <div>
+        <span class='headerFooter'>Soporte</span>
+        <div class='itemFooter'><img src='resources/img/bot.png'></div>
+
+    </div>
+    <div><span class='headerFooter'>Delegaciones</span>
+        <div class='itemFooter'><a href='#' class='aFooter'>Abu Dabi</a></div>
+        <div class='itemFooter'><a href='#' class='aFooter'>Argentina</a></div>
+        <div class='itemFooter'><a href='#' class='aFooter'>España</a></div>
+        <div class='itemFooter'><a href='#' class='aFooter'>Paraguay</a></div>
+        <div class='itemFooter'><a href='#' class='aFooter'>Portugal</a></div>
+        <div class='itemFooter'><a href='#' class='aFooter'>Reino Unido</a></div>
+        <div class='itemFooter'><a href='#' class='aFooter'>Suiza</a></div>
     </div>
 </footer>
