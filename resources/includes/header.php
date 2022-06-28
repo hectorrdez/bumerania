f4b35f6ab2566555b394555d86bd094274c34a61
pine <antonioblancocordoba96@gmail.com>
2022-06-28 12:30:35 +0200

    subida header

---
+++ resources/includes/header.php
@@ -0,0 +1,18 @@
+<body style="margin:0; padding:0; height:1000px;">
+
+<header style="background-color:black; height: 11%; align-items: center; margin: auto; display: flex;">
+    <img src="img/logo-blanco-cortado.png" alt="Bumerania logo" class="logo" style="height:65%; padding: 0 2%; margin-left: 2%;">
+    <nav style="display: flex; margin: auto; align-items: center;">
+        <div style="color:white; padding: 1% 3%; border: 1px solid red; ">INICIO</div>
+        <div style="color:white; margin:1%; padding: 1% 3%; border: 1px solid red; ">SOLUCIONES</div>
+        <div style="color:white; margin:1%; padding: 1% 3%; border: 1px solid red; ">NOSOTROS</div>
+        <div style="color:white; margin:1%; padding: 1% 3%; width: fit-content; border: 1px solid red; ">SALA DE PRENSA</div>
+        <div style="color:white; margin:1%; padding: 1% 3%; border: 1px solid red; ">CONTACTO</div>
+        <div style="color:white; margin:1%; padding: 1% 3%; border: 1px solid red; ">BLOG</div>
+        <div style="color:white; margin:1%; padding: 1% 3%; border: 1px solid red; ">DISTRIBUIDOR</div>
+        <div style="color:white; margin:1%; padding: 1% 3%; border: 1px solid red; ">SOPORTE 24/7</div>
+    </nav>
+    <div style="color:white; padding: 1%; margin: auto; border: 1px solid red; ">IDIOMA</div>
+</header>
+    
+</body>
