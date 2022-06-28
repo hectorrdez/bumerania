9d4f13b3e5657cdc2712343d3acfef79a175e8e9
pine <antonioblancocordoba96@gmail.com>
2022-06-28 13:40:09 +0200

    Header

--- resources/includes/header.php
+++ resources/includes/header.php
@@ -1,18 +1,15 @@
-<body style="margin:0; padding:0; height:1000px;">
-
-<header style="background-color:black; height: 11%; align-items: center; margin: auto; display: flex;">
-    <img src="img/logo-blanco-cortado.png" alt="Bumerania logo" class="logo" style="height:65%; padding: 0 2%; margin-left: 2%;">
-    <nav style="display: flex; margin: auto; align-items: center;">
-        <div style="color:white; padding: 1% 3%; border: 1px solid red; ">INICIO</div>
-        <div style="color:white; margin:1%; padding: 1% 3%; border: 1px solid red; ">SOLUCIONES</div>
-        <div style="color:white; margin:1%; padding: 1% 3%; border: 1px solid red; ">NOSOTROS</div>
-        <div style="color:white; margin:1%; padding: 1% 3%; width: fit-content; border: 1px solid red; ">SALA DE PRENSA</div>
-        <div style="color:white; margin:1%; padding: 1% 3%; border: 1px solid red; ">CONTACTO</div>
-        <div style="color:white; margin:1%; padding: 1% 3%; border: 1px solid red; ">BLOG</div>
-        <div style="color:white; margin:1%; padding: 1% 3%; border: 1px solid red; ">DISTRIBUIDOR</div>
-        <div style="color:white; margin:1%; padding: 1% 3%; border: 1px solid red; ">SOPORTE 24/7</div>
+<header>
+    <a href="#"><img src="../resources/img/logo-blanco-cortado.png" alt="Bumerania logo" class="logo"></a>
+    <nav>
+        <a href="#">INICIO</a >
+        <a href="#">SOLUCIONES<img src="../resources/icons/caret-down-solid.svg" alt=""></a >
+        <a href="#">NOSOTROS<img src="../resources/icons/caret-down-solid.svg" alt=""></a >
+        <a href="#">SALA DE PRENSA</a >
+        <a href="#">CONTACTO</a >
+        <a href="#">BLOG</a >
+        <a href="#">DISTRIBUIDOR</a >
+        <a href="#">SOPORTE 24/7</a >
     </nav>
-    <div style="color:white; padding: 1%; margin: auto; border: 1px solid red; ">IDIOMA</div>
+    <a href="#">IDIOMA</a>
 </header>
     
-</body>
