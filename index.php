de39c6e8ac82c8e9bcbcbdfc0b2d81cfb89885e5
hectorrdez <hectorrdez@gmail.com>
2022-07-04 12:35:25 +0200

    sass renovado

--- index.php
+++ index.php
@@ -28,42 +28,48 @@
             </article>
             <article class='category'>
                 <div class='title'>QUÉ<br>HACEMOS
-                    <div>
-                        <hr class='verdeSmall'>
+                    <div style="width: 50%;">
+                        <hr class='green'>
                     </div>
                     <div>
-                        <div class='button'><a class='boton' href='./index.php#rrss'>Conócenos</a></div>
+                        <button onclick="window.open('#rrss','_self')">Conócenos</button>
 
                     </div>
                 </div>
                 <div class='content'>
                     <div class='subtitle1'>Unimos la robótica social con la inteligencia artificial para aumentar la calidad de vida de las personas.</div>
-                    <hr class='blanco'>
+                    <hr class='white'>
                     <div class='subtitle2'>La Robótica social y la Inteligencia Artificial son la nueva revolución que marcará un antes y un después en todos los sectores de la sociedad. Apostamos por el uso y el desarrollo de esta tecnología siempre que aporte mejores soluciones para ganar en bienestar y complementar el servicio profesional.</div>
                 </div>
             </article>
         </section>
         <section>
             <article class='white'>
-                <div><img src="./resources/img/soluciones1.png" alt="soluciones"></div>
-                <div>
+                <div class="image"><img src="./resources/img/soluciones1.png" alt="soluciones"></div>
+                <div class="content">
                     <div class='title'>Soluciones</div>
-                    <hr class='verdeBig'>
+                    <div class='divisorContainer'>
+                        <div style="width: 50%;">
+                            <hr class='green'>
+                        </div>
+                    </div>
                     <div class='subtitle2'>
                         Robots dotados de inteligencia artificial que interactúan con el entorno
                     </div>
-                    <div class='paragraph'>
+                    <div class='text'>
                         Soluciones de robótica que combinan las posibilidades de la inteligencia artificial y el reconocimiento facial con el desarrollo de software personalizado y que encuentran en la robótica de servicio el soporte adecuado.
                     </div>
                 </div>
-                <div><img src="./resources/img/soluciones2.png" alt="soluciones"></div>
+                <div class="image">
+                    <img src="./resources/img/soluciones2.png" alt="soluciones">
+                </div>
             </article>
         </section>
         <section class='bg'>
             <article class='category'>
                 <div class='title'>CÓMO LO<br>HACEMOS
-                    <div>
-                        <hr class='verdeSmall'>
+                    <div style="width: 50%;">
+                        <hr class='green'>
                     </div>
                 </div>
                 <div class='content'>
@@ -75,51 +81,44 @@
         </section>
         <section>
             <article class='white'>
+                <div class='title'>Proyectos</div>
                 <div>
-                    <div class='title'>Proyectos</div>
-                    <hr class='verdeBig'>
-                    <div class='subtitle2'>
-                        Estamos especializados en ofrecer las mejores soluciones para los sectores de Hostelería, Restauración, Salud y Seguridad.
-                    </div>
+                    <hr class='green'>
                 </div>
+                <div class='subtitle2'>
+                    Estamos especializados en ofrecer las mejores soluciones para los sectores de Hostelería, Restauración, Salud y Seguridad.
+                </div>
+
             </article>
         </section>
         <section>
             <div class="accordion">
                 <div class="box">
                     <img src="/resources/img/acordeon/acordeon1.jpg">
-                    <div>
-                        <img src="/resources/img/acordeon/acordeon1.svg">HOSTELERÍA
-                        <div class='button'>
-                            <a class='boton'>Mas información</a>
-                        </div>
+                    <div class="content">
+                        <img src="/resources/img/acordeon/acordeon1.svg">HOSTELERÍA<br>
+                        <button>Más información</button>
                     </div>
                 </div>
                 <div class="box">
                     <img src="/resources/img/acordeon/acordeon2.jpg">
-                    <div>
-                        <img src="/resources/img/acordeon/acordeon2.svg">RESTAURACIÓN
-                        <div class='button'>
-                            <a class='boton'>Mas información</a>
-                        </div>
+                    <div class="content">
+                        <img src="/resources/img/acordeon/acordeon2.svg">RESTAURACIÓN<br>
+                        <button>Más información</button>
                     </div>
                 </div>
                 <div class="box">
                     <img src="/resources/img/acordeon/acordeon3.jpg">
-                    <div>
-                        <img src="/resources/img/acordeon/acordeon3.svg">SALUD
-                        <div class='button'>
-                            <a class='boton'>Mas información</a>
-                        </div>
+                    <div class="content">
+                        <img src="/resources/img/acordeon/acordeon3.svg">SALUD <br>
+                        <button>Más información</button>
                     </div>
                 </div>
                 <div class="box">
                     <img src="/resources/img/acordeon/acordeon4.jpg">
-                    <div>
-                        <img src="/resources/img/acordeon/acordeon4.svg">SEGURIDAD
-                        <div class='button'>
-                            <a class='boton'>Mas información</a>
-                        </div>
+                    <div class="content">
+                        <img src="/resources/img/acordeon/acordeon4.svg">SEGURIDAD<br>
+                        <button>Más información</button>
                     </div>
                 </div>
             </div>
