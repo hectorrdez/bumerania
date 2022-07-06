b6b7afeb63ef91f66ccee4b0aff98ae8c055f824
Xcese <tute_vdv@hotmail.com>
2022-07-06 10:48:01 +0200

    main.js arreglaico

--- resources/code/main.js
+++ resources/code/main.js
@@ -1,6 +1,6 @@
 document.querySelector('body').style.overflow = 'hidden';
 window.addEventListener('load', () => {
-    document.querySelector('body').style.overflow = 'scroll';
+    document.querySelector('body').style.overflow = 'auto';
     preloader.style.display = 'none';
 });
 
